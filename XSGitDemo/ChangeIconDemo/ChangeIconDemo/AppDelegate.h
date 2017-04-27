02e87a0017b6ee5f081f694f485f6d0fce28d985
iOS－Dev <592722776@qq.com>
2017-04-27 17:37:42 +0800

    no message

---
+++ XSGitDemo/ChangeIconDemo/ChangeIconDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChangeIconDemo
+//
+//  Created by iOS－Dev on 2017/4/26.
+//  Copyright © 2017年 iOS－Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
